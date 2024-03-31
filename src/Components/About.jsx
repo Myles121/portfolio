e36b2a13b3812e24c35b851efd3a69630ab91177
reii <124282607+Myles121@users.noreply.github.com>
2024-03-31 16:34:01 +0000

    updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student at ACLC College of Butuan, specializing in web development and programming. I prioritize user-friendly and accessible web applications, always eager to learn and innovate. While I have minimal design experience, my focus is on solving problems through coding and driving technological accessibility.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
+  "Programmer",
+  "Web Developer",
   "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Continuous Learning",
+  "Database Management",
+  "Problem Solving",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Success is not just about reaching the destination; it's about embracing the journey with resilience, learning, and a relentless pursuit of improvement.";
 
 const About = () => {
   return (
